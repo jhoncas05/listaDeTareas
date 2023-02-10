5d38e324c95c093e87ac15372f57490fc96ab4aa
jhoncas05 <jhoncas.mods@gmail.com>
2023-02-10 10:53:15 -0500

    se termina la app

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -5,7 +5,7 @@ function EmptyTodos(){
     return(
 
         <div className="container" id="contenedorNoDisponible">
-            <img id="imgNoDisponible" src="http://localhost:3000/2721299.png" alt="NoDisponible"></img>  
+            <img id="imgNoDisponible" src="https://firebasestorage.googleapis.com/v0/b/react-images-bf61b.appspot.com/o/2721299.png?alt=media&token=6c7c535b-178b-4c83-b91a-6156777979ec" alt="NoDisponible"></img>  
             <p id="textNoDisponible"><strong>No tienes tareas pendientes en este momento.</strong></p>
         </div>
     
--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -6,7 +6,7 @@ function TodosLoading(){
         <>
 
         <div className="LoadingTodo-container">
-            <img id="logoCargando" className="LoadingTodo-completeIcon LoadingTodo-text LoadingTodo-deleteIcon" src="http://localhost:3000/3579899.png" alt="estamos cargando..."></img>
+            <img id="logoCargando" className="LoadingTodo-completeIcon LoadingTodo-text LoadingTodo-deleteIcon" src="https://firebasestorage.googleapis.com/v0/b/react-images-bf61b.appspot.com/o/3579899.png?alt=media&token=665cbd7c-7804-4784-969d-2b3b69d81795" alt="estamos cargando..."></img>
         </div>
 
         </>
