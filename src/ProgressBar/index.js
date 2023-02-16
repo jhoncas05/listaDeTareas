afcbd952d2bb8545b8d166f368b99004ba1469ff
jhoncas05 <jhoncas.mods@gmail.com>
2023-02-16 10:51:00 -0500

    nuevos estilos barra de progreso

--- src/ProgressBar/index.js
+++ src/ProgressBar/index.js
@@ -5,9 +5,10 @@ import './progressBar.css'
 function ProgressBar() {
     const {totalTodos, todosCompletos} = React.useContext(TodoContext);
     return(
-        <div id="progress">
-            <progress id="progress-bar" max={totalTodos} value={todosCompletos}>
-                {/* <span class="progress-bar-text">75%</span> */}
+
+        <div id="contenedorProgress">
+            <progress id="progressBar" max={totalTodos} value={todosCompletos}>
+                <span class="progressBarText">75%</span>
             </progress>
         </div>
         
